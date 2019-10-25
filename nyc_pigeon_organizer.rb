34de5093d7b63a7dfa219ba915ccf77fab12cdb9
Vivian Tran <viviantran94@gmail.com>
2019-10-25 03:18:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |color_gender_lives, value|
+  data.each do |attir, value|
     value.each do |stats, all_names|
       all_names.each do |name|
         if pigeon_list[name] == nil
